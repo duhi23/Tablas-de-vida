7cddbc5146e2f4a1d028029ffbebdde7579edb2b
Diego Huaraca <diego.21.phs@hotmail.com>
2015-06-03 14:37:25 -0500

    Script Datos

---
+++ script_data.R
@@ -0,0 +1,13 @@
+### Tablas de mortalidad ###
+###      Script Data     ###
+############################
+
+dir.pro <- "/Users/Diego/Desktop/Paper/Tablas Mortalidad"
+dir.data <- "/Users/Diego/Desktop/Paper/Datos"
+
+setwd(dir.data)
+list.files()
+
+library(haven)
+library(data.table)
+library(dplyr)
